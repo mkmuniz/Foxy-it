b0565e949fc91c194358109156b433e663b1aad9
worstp <mikaelmuniz2001@gmail.com>
2022-08-16 15:55:33 -0300

    feat: adding configs to connect database

---
+++ back/configs/config.go
@@ -0,0 +1,64 @@
+package configs
+
+import "github.com/spf13/viper"
+
+var cfg *config
+
+type config struct {
+	API APIConfig
+	DB  DBConfig
+}
+
+type APIConfig struct {
+	Port string
+}
+
+type DBConfig struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	Name     string
+}
+
+func init() {
+	viper.SetDefault("api.port", ":4000")
+	viper.SetDefault("db.host", "localhost")
+	viper.SetDefault("db.port", "5432")
+}
+
+func Load() error {
+	viper.SetConfigType("env")
+	viper.AddConfigPath(".")
+	err := viper.ReadInConfig()
+
+	if err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
+			return err
+		}
+	}
+
+	cfg = new(config)
+
+	cfg.API = APIConfig{
+		Port: viper.GetString("api.port"),
+	}
+
+	cfg.DB = DBConfig{
+		Host:     viper.GetString("DB_HOST"),
+		Port:     viper.GetString("DB_PORT"),
+		User:     viper.GetString("DB_USER"),
+		Password: viper.GetString("DB_PASSWORD"),
+		Name:     viper.GetString("DB_NAME"),
+	}
+
+	return nil
+}
+
+func GetConfig(config string) interface{} {
+	if config == "api" {
+		return cfg.API.Port
+	}
+
+	return cfg.DB
+}
