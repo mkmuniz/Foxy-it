ee90bd217b169bcc8c100016c1f3d400147ba811
worstp <mikaelmuniz2001@gmail.com>
2022-08-16 15:56:35 -0300

    feat: changing template's project from Vue.js to React TS

---
+++ front/src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css'
+import MenuAppBar from './ui/components/NavBar'
+
+function App() {
+  return (
+    <MenuAppBar />
+  )
+}
+
+export default App
---
+++ front/src/routes/index.tsx
@@ -0,0 +1,7 @@
+import { Route } from 'react-router-dom';
+
+export default function Routes() {
+    return (
+        <div>Teste</div>
+    );
+}
