265e2f3d25f015a129caf85ae357fe9b657b522c
worstp <mikaelmuniz2001@gmail.com>
2022-09-09 15:54:47 -0300

    feat: creating a High-Order component verify if user is authenticated

--- front/utils/auth/withoutAuth.tsx
+++ front/utils/auth/withoutAuth.tsx
@@ -2,14 +2,14 @@ import { useRouter } from "next/router";
 import { ElementType, useEffect } from "react";
 import Cookie from 'js-cookie';
 
-export default function withAuth(WrappedComponent: ElementType) {
+export default function withoutAuth(WrappedComponent: ElementType) {
     const Wrapper = (props: unknown) => {
         const router = useRouter();
-
+        
         useEffect(() => {
             const token = Cookie.get('token');
 
-            if (!token) router.replace('/login')
+            if(token) router.replace('/')
         }, [])
 
         return <WrappedComponent />
