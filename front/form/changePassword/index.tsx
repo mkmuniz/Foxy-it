0807187be1e631417b2bcc45acc1007e4c6d1401
worstp <mikaelmuniz2001@gmail.com>
2022-09-11 23:42:09 -0300

    feat: creating a button to 'change password' form

--- front/form/changePassword/index.tsx
+++ front/form/changePassword/index.tsx
@@ -1,24 +1,27 @@
-import { Typography, TextField } from '@mui/material';
+import { Typography, TextField, Button, FormControl } from '@mui/material';
 import React, { useContext } from 'react';
 import { AuthContext } from '../../auth/provider';
 import { TextFieldStyle } from '../login/style';
 
 export default function ChangePassword() {
     return <>
-        <Typography variant="h4">
-            Your password
-        </Typography>
-        <TextField sx={TextFieldStyle}
-            required
-            id="outlined-required"
-            label="Password"
-            name="name"
-        />
-        <TextField sx={TextFieldStyle}
-            required
-            id="outlined-required"
-            label="New password"
-            name="email"
-        />
+        <FormControl sx={{ alignItems: 'center', width: '75%' }}>
+            <Typography variant="h4">
+                Your password
+            </Typography>
+            <TextField sx={TextFieldStyle}
+                required
+                id="outlined-required"
+                label="Password"
+                name="name"
+            />
+            <TextField sx={TextFieldStyle}
+                required
+                id="outlined-required"
+                label="New password"
+                name="email"
+            />
+            <Button variant="contained">Save</Button>
+        </FormControl>
     </>
 }
