f6ca43f644a565bf986ab881fde611dcc5865700
worstp <mikaelmuniz2001@gmail.com>
2022-09-21 19:02:33 -0300

    feat: adjusting content in footer

--- front/components/footer/index.tsx
+++ front/components/footer/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Grid, BottomNavigation, Typography, ListItem, ListItemText, List } from '@mui/material';
+import { Grid, BottomNavigation, Typography } from '@mui/material';
 import { BoxFooterStyle } from './style';
 
 export default function Footer() {
@@ -7,66 +7,9 @@ export default function Footer() {
         <BottomNavigation sx={BoxFooterStyle}>
             <Grid container>
                 <Grid xs={4}>
-                    <Typography variant='h4'>
-                        About
+                    <Typography variant='h6'>
+                        @Copyright 2022
                     </Typography>
-                    <List>
-                        <ListItem>
-                            <ListItemText>
-                                Teste
-                            </ListItemText>
-                        </ListItem>
-                        <ListItem>
-                            <ListItemText>
-                                Teste
-                            </ListItemText>
-                        </ListItem>
-                        <ListItem>
-                            <ListItemText>
-                                Teste
-                            </ListItemText>
-                        </ListItem>
-                    </List>
-                </Grid>
-                <Grid xs={4}>
-                    <Typography variant='h4'>
-                        Contact
-                    </Typography>
-                    <ListItem>
-                        <ListItemText>
-                            Teste
-                        </ListItemText>
-                    </ListItem>
-                    <ListItem>
-                        <ListItemText>
-                            Teste
-                        </ListItemText>
-                    </ListItem>
-                    <ListItem>
-                        <ListItemText>
-                            Teste
-                        </ListItemText>
-                    </ListItem>
-                </Grid>
-                <Grid xs={3}>
-                    <Typography variant='h4'>
-                        Team
-                    </Typography>
-                    <ListItem>
-                        <ListItemText>
-                            Teste
-                        </ListItemText>
-                    </ListItem>
-                    <ListItem>
-                        <ListItemText>
-                            Teste
-                        </ListItemText>
-                    </ListItem>
-                    <ListItem>
-                        <ListItemText>
-                            Teste
-                        </ListItemText>
-                    </ListItem>
                 </Grid>
             </Grid>
         </BottomNavigation>
