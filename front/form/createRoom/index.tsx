5e71ad940ab7f4c7195eba5ff611bacc78659153
worstp <mikaelmuniz2001@gmail.com>
2022-09-22 18:02:19 -0300

    feat: adding fields inside 'Create Room' modal

--- front/form/createRoom/index.tsx
+++ front/form/createRoom/index.tsx
@@ -1,16 +1,41 @@
-import { Box, Card, CardContent, Grid, Typography } from '@mui/material';
-import React from 'react';
+import { Box, Card, CardContent, Grid, Typography, CardMedia, Button, FormControl, TextField } from '@mui/material';
+import React, { useState } from 'react';
+import { TextFieldStyle } from '../login/style';
+
+type Form = {
+    name: string,
+}
 
 export default function CreateRoom() {
+    const [form, setForm] = useState({} as Form);
+
+    const getValues = (e: any) => {
+        setForm({ ...form, [e.target.name]: e.target.value });
+    };
+
+    console.log(form);
+    
     return <>
         <Box>
-            <Grid container spacing={2} direction="column" alignItems="center" sx={{ mt: '10%'}}>
-                <Grid item xs={12} justifyContent="center">
+            <Grid container spacing={2} direction="column" alignItems="center" sx={{ mt: '10%' }}>
+                <Grid item xs={12} justifyContent="center" sx={{ width: '40% '}}>
                     <Card>
-                        <CardContent>
-                            <Typography variant='h6'>
-                                Create room
+                        <CardMedia sx={{ textAlign: 'center'}}>
+                            <Typography variant='h5'>
+                                Create a Room
                             </Typography>
+                        </CardMedia>
+                        <CardContent>
+                            <FormControl sx={{ alignItems: 'center', width: '100%' }}>
+                                <TextField sx={TextFieldStyle}
+                                    required
+                                    id="outlined-required"
+                                    label="Room name"
+                                    name="name"
+                                    onChange={getValues}
+                                />
+                                <Button variant="contained">Create</Button>
+                            </FormControl>
                         </CardContent>
                     </Card>
                 </Grid>
