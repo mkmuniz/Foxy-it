9989cb24f44db53f0eff5146f9337c836eb9b751
worstp <mikaelmuniz2001@gmail.com>
2022-09-24 12:10:29 -0300

    fix: modifying 'room' routes in next server-side

--- front/form/createRoom/index.tsx
+++ front/form/createRoom/index.tsx
@@ -1,19 +1,26 @@
 import { Box, Card, CardContent, Grid, Typography, CardMedia, Button, FormControl, TextField } from '@mui/material';
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import MessageTemplate from '../../components/message';
 import { TextFieldStyle } from '../login/style';
 import { createRoom } from '../../requests/room';
+import { createParticipation } from '../../requests/participation';
+import { AuthContext } from '../../auth/provider';
+import { updateUser } from '../../requests/user';
 
 type Form = {
     name: string,
-}
+};
 
 export default function CreateRoom() {
     const [form, setForm] = useState({} as Form);
+    const { user } = useContext(AuthContext);
+
     const [feedback, setFeedback] = useState({
         status: "",
         description: ""
     })
+
+    console.log(user);
 
     const getValues = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
         setForm({ ...form, [e.target.name]: e.target.value });
@@ -23,7 +30,16 @@ export default function CreateRoom() {
         e.preventDefault();
 
         try {
-            await createRoom(form);
+            let room = await createRoom(form);
+            let participation = await createParticipation({
+                user_id: user.ID,
+                name: user.Name,
+                room: room.id,
+                permission: "owner"
+            });
+            let participations = [];
+
+            await updateUser(user.ID, { participations: participations.push(participation.id)});
 
             return setFeedback({
                 status: "success",
