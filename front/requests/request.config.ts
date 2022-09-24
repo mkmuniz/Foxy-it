1a3f652efae26fa5f4098377846bb64a6cf9fb60
worstp <mikaelmuniz2001@gmail.com>
2022-09-24 12:10:58 -0300

    feat: modifying requests configs and adding a function method to 'UPDATE'

--- front/requests/request.config.ts
+++ front/requests/request.config.ts
@@ -34,3 +34,23 @@ export async function get(url: string) {
         throw err;
     }
 };
+
+export async function patch(url: string, body: any) {
+    try {
+        const response = await fetch('http://localhost:3000/api' + url, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+            },
+            body: JSON.stringify(body)
+        });
+
+        const data = await response.json();
+        
+        return data;
+    } catch (err) {
+        console.log(err);
+        throw err;
+    }
+};
--- front/requests/user.ts
+++ front/requests/user.ts
@@ -1,8 +1,8 @@
-import { post } from './request.config';
+import { post, patch } from './request.config';
 
 export async function createUser(body: any) {
     try {
-        const res = await post('/user', body);
+        const res = await post('/user/create', body);
         return { ...res};
     } catch (err) {
         console.log(err);
@@ -19,3 +19,13 @@ export async function login(body: any) {
         return err;
     }
 };
+
+export async function updateUser(id: string, body: any) {
+    try {
+        const res = await patch(`/user/update/${id}`, body);
+        return { ...res};
+    } catch (err) {
+        console.log(err);
+        return err;
+    }
+}
