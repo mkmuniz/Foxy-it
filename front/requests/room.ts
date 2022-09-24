8f4e7223376c1be0ebc486a89872d087839f78c6
worstp <mikaelmuniz2001@gmail.com>
2022-09-23 22:20:51 -0300

    feat: creating an api route to create a room

--- front/requests/room.ts
+++ front/requests/room.ts
@@ -2,7 +2,7 @@ import { post } from './request.config';
 
 export async function createRoom(body: any) {
     try {
-        const res = await post('/room/room', body);
+        const res = await post('/room/create', body);
         return { ...res};
     } catch (err) {
         console.log(err);
