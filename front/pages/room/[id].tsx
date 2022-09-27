0b3ba623834050d2194773433190af8db648cdf7
worstp <mikaelmuniz2001@gmail.com>
2022-09-27 11:25:06 -0300

    feat: creating room prototype

--- front/pages/room/[id].tsx
+++ front/pages/room/[id].tsx
@@ -1,12 +1,29 @@
-import { Container, Box } from '@mui/material';
+import { Box, Grid, Container } from '@mui/material';
 import React from 'react';
+import Navbar from '../../components/navbar';
+import { ChatStyle, ScreenStyle, UserStyle } from './style';
 
 export default function Room() {
     return <>
         <Box>
-            <Container>
-                <h1>Screen</h1>
-            </Container>
+            <Navbar />
+            <Grid container sx={{ alignItems: 'center', textAlign: 'center' }}>
+                <Grid xs={8}>
+                    <Container sx={ScreenStyle}>
+                        Teste
+                    </Container>
+                    <Container sx={UserStyle}>
+                        <Box>
+                            Users
+                        </Box>
+                    </Container>
+                </Grid>
+                <Grid xs={4}>
+                    <Box sx={ChatStyle}>
+                        Chat
+                    </Box>
+                </Grid>
+            </Grid>
         </Box>
     </>
 }
---
+++ front/pages/room/style.ts
@@ -0,0 +1,17 @@
+export const ScreenStyle = {
+    minHeight: '70vh',
+    height: '100%',
+    backgroundColor: 'red'
+};
+
+export const UserStyle = {
+    height: '100%',
+    minHeight: '30vh',
+    backgroundColor: 'blue'
+};
+
+export const ChatStyle = {
+    height: '100%',
+    minHeight: '100vh',
+    backgroundColor: 'green'
+}
